b81dd621c69dec152dcfd6cd66d4654221b17409
Leehyoju97 <dlgywn2420@gmail.com>
2022-11-12 07:36:16 +0900

    initial commit

---
+++ src/main/java/com/leehyoju97/shortenurl/ShortenurlApplication.java
@@ -0,0 +1,13 @@
+package com.leehyoju97.shortenurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leehyoju97/shortenurl/ShortenurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leehyoju97.shortenurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
